fc6191498f5a97e10038dafc289fc14b2a6d2691
xeroso <xeroso>
2021-03-31 21:50:15 -0300

    dasdad

--- lib/tiled_component.dart
+++ lib/tiled_component.dart
@@ -1,21 +1,27 @@
 import 'package:flame/components.dart';
+import 'package:flame/sprite.dart';
 import 'package:tiled/tiled.dart';
 
 import 'dart:ui';
 
 import './tiled.dart';
 
-class TiledComponent extends Component {
+class TiledComponent extends IsometricTileMapComponent {
+  final SpriteSheet tileset;
+  final List<List<int>> matrix;
+
   Tiled _tiled;
 
-  TiledComponent(String filename, Size destTileSize) {
+  TiledComponent(String filename, Size destTileSize,{this.matrix, this.tileset}) : super(tileset, matrix) {
     _tiled = Tiled(filename, destTileSize);
   }
 
-  TiledComponent.fromTiled(this._tiled);
+  TiledComponent.fromTiled(this._tiled,{this.matrix, this.tileset}) :  super(tileset, matrix) ;
 
   @override
-  void update(double dt) {}
+  void update(double dt) {
+
+  }
 
   @override
   void render(Canvas canvas) {
